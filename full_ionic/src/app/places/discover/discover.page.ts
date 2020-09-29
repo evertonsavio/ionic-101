d1d3d534c51b46828020e59f99bf9e751a89946f
havyx <everluca@outlook.com>
2020-09-29 10:47:59 -0300

    :boom: virtual scrolling

--- full_ionic/src/app/places/discover/discover.page.ts
+++ full_ionic/src/app/places/discover/discover.page.ts
@@ -10,6 +10,8 @@ import { PlacesService } from "../places.service";
 })
 export class DiscoverPage implements OnInit {
   loadedPlaces: Place[];
+  listedLoadedPlaces: Place[];
+
   constructor(
     private placesService: PlacesService,
     private menuCtrl: MenuController
@@ -17,6 +19,7 @@ export class DiscoverPage implements OnInit {
 
   ngOnInit() {
     this.loadedPlaces = this.placesService.places;
+    this.listedLoadedPlaces = this.loadedPlaces.slice(1);
   }
   onClick() {
     this.menuCtrl.toggle();
