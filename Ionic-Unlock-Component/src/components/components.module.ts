32bdcbc62dfedf8a57db127e31339dc14fc02d35
havyx <everluca@outlook.com>
2020-09-30 15:17:13 -0300

    :hammer:refactoring

--- Ionic-Unlock-Component/src/components/components.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { Unlocker } from './unlocker/unlocker';
-@NgModule({
-	declarations: [Unlocker],
-	imports: [],
-	exports: [Unlocker]
-})
-export class ComponentsModule {}
