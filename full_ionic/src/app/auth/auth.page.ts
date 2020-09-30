15e05efce49f87ac574063fbfb77a6d3f8d67463
havyx <everluca@outlook.com>
2020-09-30 11:35:57 -0300

    :boom: login/logout form template

--- full_ionic/src/app/auth/auth.page.ts
+++ full_ionic/src/app/auth/auth.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
+import { NgForm } from "@angular/forms";
 import { LoadingController } from "@ionic/angular";
+
 import { AuthService } from "./auth.service";
 
 @Component({
@@ -9,22 +11,22 @@ import { AuthService } from "./auth.service";
   styleUrls: ["./auth.page.scss"],
 })
 export class AuthPage implements OnInit {
+  isLoading = false;
+  isLogin = true;
+
   constructor(
     private authService: AuthService,
     private router: Router,
     private loadingCtrl: LoadingController
   ) {}
 
-  isLoading = false;
-
   ngOnInit() {}
 
   onLogin() {
-    //Loaging Controller
     this.isLoading = true;
     this.authService.login();
     this.loadingCtrl
-      .create({ keyboardClose: true, message: "logging In..." })
+      .create({ keyboardClose: true, message: "Logging in..." })
       .then((loadingEl) => {
         loadingEl.present();
         setTimeout(() => {
@@ -33,12 +35,25 @@ export class AuthPage implements OnInit {
           this.router.navigateByUrl("/places/discover");
         }, 1500);
       });
+  }
 
-    // Loading Ion-Spinner
-    this.isLoading = true;
-    setTimeout(() => {
-      this.isLoading = false;
-      this.router.navigateByUrl("/places/discover");
-    }, 1500);
+  onSwitchAuthMode() {
+    this.isLogin = !this.isLogin;
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(form);
+    if (!form.valid) {
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+    console.log(email, password);
+
+    if (this.isLogin) {
+      // Send a request to login servers
+    } else {
+      // Send a request to signup servers
+    }
   }
 }
