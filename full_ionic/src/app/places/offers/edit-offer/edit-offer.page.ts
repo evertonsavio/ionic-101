782e2b7088c96cba60dab40ee8175ffa5451a249
Everton Sávio S. Lucas <everluca@outlook.com>
2020-10-03 13:49:45 -0300

    :sparkles: error handler

--- full_ionic/src/app/places/offers/edit-offer/edit-offer.page.ts
+++ full_ionic/src/app/places/offers/edit-offer/edit-offer.page.ts
@@ -1,10 +1,15 @@
 import { Component, OnDestroy, OnInit } from "@angular/core";
 import { FormControl, FormGroup, Validators } from "@angular/forms";
 import { ActivatedRoute, Router } from "@angular/router";
-import { LoadingController, NavController } from "@ionic/angular";
+import {
+  LoadingController,
+  NavController,
+  AlertController,
+} from "@ionic/angular";
 import { PlacesService } from "../../places.service";
 import { Place } from "../../place.model";
 import { Subscription } from "rxjs";
+import { error } from "@angular/compiler/src/util";
 
 @Component({
   selector: "app-edit-offer",
@@ -12,19 +17,20 @@ import { Subscription } from "rxjs";
   styleUrls: ["./edit-offer.page.scss"],
 })
 export class EditOfferPage implements OnInit, OnDestroy {
-  place: Place;
-  placeId: string;
-  form: FormGroup;
-  isLoading: boolean = false;
-  private placeSub: Subscription;
-
   constructor(
     private route: ActivatedRoute,
     private placesService: PlacesService,
     private navCtrl: NavController,
     private router: Router,
-    private loadingCtrl: LoadingController
+    private loadingCtrl: LoadingController,
+    private alertCtrl: AlertController
   ) {}
+
+  place: Place;
+  placeId: string;
+  form: FormGroup;
+  isLoading: boolean = false;
+  private placeSub: Subscription;
 
   ngOnInit() {
     this.route.paramMap.subscribe((paramMap) => {
@@ -36,20 +42,40 @@ export class EditOfferPage implements OnInit, OnDestroy {
       this.isLoading = true;
       this.placeSub = this.placesService
         .getPlace(paramMap.get("placeId"))
-        .subscribe((place) => {
-          this.place = place;
-          this.form = new FormGroup({
-            title: new FormControl(this.place.title, {
-              updateOn: "blur",
-              validators: [Validators.required],
-            }),
-            description: new FormControl(this.place.description, {
-              updateOn: "blur",
-              validators: [Validators.required, Validators.maxLength(180)],
-            }),
-          });
-          this.isLoading = false;
-        });
+        .subscribe(
+          (place) => {
+            this.place = place;
+            this.form = new FormGroup({
+              title: new FormControl(this.place.title, {
+                updateOn: "blur",
+                validators: [Validators.required],
+              }),
+              description: new FormControl(this.place.description, {
+                updateOn: "blur",
+                validators: [Validators.required, Validators.maxLength(180)],
+              }),
+            });
+            this.isLoading = false;
+          },
+          (error) => {
+            this.alertCtrl
+              .create({
+                header: "Um erro occorreu",
+                message: "OOOPS",
+                buttons: [
+                  {
+                    text: "Okay",
+                    handler: () => {
+                      this.router.navigate(["/places/offers"]);
+                    },
+                  },
+                ],
+              })
+              .then((alertEl) => {
+                alertEl.present();
+              });
+          }
+        );
     });
   }
 
